f3b9c4f6deebdef76b55b97a772e87f7925913ed
Nicollas Anderson <nicollas_sdias@hotmail.com>
2023-07-15 16:08:48 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: './runtimeConfig',
-        replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
-      },
-    ]
-  }
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
 })
